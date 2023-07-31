6a1f6d57148b81e3fe0a2deffd8ddf905387243e
percy1997 <percy_01_1997@outlook.com>
2023-07-30 20:00:50 -0500

    registrar material y validaciones

--- src/main/java/com/gourmet/controller/MaterialController.java
+++ src/main/java/com/gourmet/controller/MaterialController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,4 +48,12 @@ public class MaterialController {
 			return matSer.materialPorAerolinea(codAer);
 		}
 	}
+	
+	@PostMapping("/registrar")
+	public String registrarMaterial(Material material) {
+		matSer.registrar(material);
+		return "redirect:/material";
+	}
+	
+	
 }
