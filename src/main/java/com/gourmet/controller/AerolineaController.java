5dc10d14c3ab5d4917da1bbf28f19f458f7d31d4
percy1997 <percy_01_1997@outlook.com>
2023-08-01 19:28:31 -0500

    pagina aerolinea, creacion AerolineaController, listar aerolineas, cambios en boton registrar aerolinea

--- src/main/java/com/gourmet/controller/AerolineaController.java
+++ src/main/java/com/gourmet/controller/AerolineaController.java
@@ -1,5 +1,30 @@
 package com.gourmet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gourmet.service.AerolineaServices;
+
+@Controller
+@RequestMapping("/aerolinea")
 public class AerolineaController {
 
+	@Autowired
+	private AerolineaServices aerSer;
+	
+	//listar aerolineas
+	@RequestMapping("")
+	public String lista(Model model) {
+		model.addAttribute("aerolineas",aerSer.listar());
+		return "aerolinea";
+	}
+	
+	//registrar una aerolinea
+	@PostMapping("/registrar")
+	public String registrarAerolinea() {
+		return "aerolinea";
+	}
 }
