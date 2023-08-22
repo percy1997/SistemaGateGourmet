cf4dd2c148bf8db8cbcc53a036d6f6fbcf95f304
percy1997 <percy_01_1997@outlook.com>
2023-08-21 22:54:46 -0500

    Mensajes confirmacion, funcion actualizar airport funcion eliminar incompleta

--- src/main/java/com/gourmet/controller/AeropuertoController.java
+++ src/main/java/com/gourmet/controller/AeropuertoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gourmet.entity.Aeropuerto;
 import com.gourmet.entity.Pais;
@@ -50,20 +51,35 @@ public class AeropuertoController {
 	}
 	
 	@PostMapping("/guardarPais")
-	public String guardarPais(Pais p) {
+	public String guardarPais(Pais p, RedirectAttributes redirect) {
 		paiSer.registrarPais(p);
+		redirect.addFlashAttribute("MENSAJE","País Registrado");
 		return "redirect:/aeropuerto";
 	}
 	
 	@PostMapping("/guardarRegion")
-	public String guardarRegion(Region r) {
+	public String guardarRegion(Region r, RedirectAttributes redirect) {
 		regSer.registrar(r);
+		redirect.addFlashAttribute("MENSAJE","Región Registrada");
 		return "redirect:/aeropuerto";
 	}
 	
 	@PostMapping("/guardarAeropuerto")
-	public String guardarAeropuerto(Aeropuerto a) {
+	public String guardarAeropuerto(RedirectAttributes redirect, Aeropuerto a, @RequestParam("codigoAeropuerto") int codigo) {
+		if(codigo == 0) {
 		aerSer.registrarAeropuerto(a);
+		redirect.addFlashAttribute("MENSAJE","Aeropuerto Registrado");
+		} 
+		else {
+		aerSer.actualizar(a);
+		redirect.addFlashAttribute("MENSAJE","Aeropuerto Actualizado");
+		}
+		return "redirect:/aeropuerto";
+	}
+	
+	@RequestMapping("/eliminarAeropuerto")
+	public String eliminarAeropuerto(@RequestParam("codigoEliminar")int cod) {
+		aerSer.eliminar(cod);
 		return "redirect:/aeropuerto";
 	}
 	
--- src/main/java/com/gourmet/service/AeropuertoServices.java
+++ src/main/java/com/gourmet/service/AeropuertoServices.java
@@ -14,19 +14,28 @@ public class AeropuertoServices {
 	@Autowired
 	private AeropuertoRepository repo;
 	
+	//listar aeropuertos
 	public List<Aeropuerto> listaAeropuertos(){
 		return repo.findAll();
 	}
 	
+	//registrar
 	public void registrarAeropuerto(Aeropuerto a) {
 		repo.save(a);
 	}
 	
+	//actualizar
 	public void actualizar(Aeropuerto a) {
 		repo.save(a);
 	}
 	
+	//buscar aeropuerto por codigo
 	public Aeropuerto buscarAeropuerto(int cod) {
 		return repo.findById(cod).orElse(null);
+	}
+	
+	//eliminar aeropuerto por codigo
+	public void eliminar(Integer cod) {
+		repo.deleteById(cod);
 	}
 }
